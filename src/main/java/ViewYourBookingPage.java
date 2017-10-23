6cb8392bbe13e04716411e04f2de9a97c2b3aea2
AlesiaK <leysan.leysan@gmail.com>
2017-10-24 01:43:39 +0300

    ViewBookingPage methods added.

--- src/main/java/ViewYourBookingPage.java
+++ src/main/java/ViewYourBookingPage.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
+
 import java.util.List;
 
 
@@ -17,17 +19,29 @@ public class ViewYourBookingPage {
     @FindBy() private WebElement duration;
     @FindBy(xpath = "//h3//span[@class='nowrap']") private List<WebElement> flyingRoute;
 
-
-
-
     public ViewYourBookingPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    public String getDepartureTime(){
+        return deptAndArrivalTimes.get(0).getAttribute("value");
+    }
 
+    public String getArrivalTime(){
+        return deptAndArrivalTimes.get(1).getAttribute("value");
+    }
 
+    public String getFlyingFrom(){
+        return flyingRoute.get(0).getText();
+    }
 
+    public String getFlyingTo(){
+        return flyingRoute.get(1).getText();
+    }
+
+   /* String s ="23:10";
+    Time.valueOf(s);*/
 
 
 }
--- src/test/ManageBookingTests/ViewBookingWithoutAccountTests.java
+++ src/test/ManageBookingTests/ViewBookingWithoutAccountTests.java
@@ -1,10 +1,11 @@
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ViewBookingWithoutAccountTests extends BaseTest {
 
     @Test (dataProvider = "viewBooking_WithoutAccount_Provider")
-    public void viewBooking_WithoutAccount_ArrivalTime_IsCorrect(String bookingNumber, String lastName, String flightDate){
+    public void viewBooking_WithoutAccount_ArrivalTime_IsCorrect(String bookingNumber, String lastName, String flightDate, String flyingFrom, String flyingTo){
         HomePageTests homePageTests = new HomePageTests(driver,homePage);
         homePageTests.testOpenManageBookingToolbar();
         homePageTests.testGoToViewBooking();
@@ -16,6 +17,11 @@ public class ViewBookingWithoutAccountTests extends BaseTest {
         loginPageTests.testFLightDateFillsCorrectly(flightDate);
         loginPage.submitFlightDateAndGoToViewBooking(); //cannot click View button as it's overlapped with opened calendar, thus using Enter in flightDate field
         ViewYourBookingPage viewYourBookingPage = new ViewYourBookingPage(driver);
+        Assert.assertEquals(viewYourBookingPage.getFlyingFrom(), flyingFrom, "Flying From destination does not meet to the expected: " +flyingFrom);
+        Assert.assertEquals(viewYourBookingPage.getFlyingTo(), flyingTo, "Flying To destination does not meet to the expected: " +flyingTo);
+        Assert.assertNotNull(viewYourBookingPage.getDepartureTime(), "");
+        Assert.assertNotNull(viewYourBookingPage.getArrivalTime(), "");
+
 
 
         String s = "";
@@ -26,7 +32,7 @@ public class ViewBookingWithoutAccountTests extends BaseTest {
     @DataProvider(name = "viewBooking_WithoutAccount_Provider")
     public Object[][] viewBooking_WithoutAccount_Provider() {
             return new Object[][]{
-                    {"MF8C9R", "kukharau", "09 June 2016"},
+                    {"MF8C9R", "kukharau", "09 June 2016", "Pisa", "Amsterdam(Shiphol)"},
             };
     }
 
