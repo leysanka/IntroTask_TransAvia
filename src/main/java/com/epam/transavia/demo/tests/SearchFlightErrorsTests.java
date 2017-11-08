32374d00b25a56569147c688a8731f2c71334c33
Alesia Kastsiuchenka <alesia_kastsiuchenka@epam.com>
2017-11-08 14:56:15 +0300

    SearchFlightErrorsTests -> cosmetic changes.

---
+++ src/main/java/com/epam/transavia/demo/tests/SearchFlightErrorsTests.java
@@ -0,0 +1,29 @@
+package com.epam.transavia.demo.tests;
+
+import com.epam.transavia.demo.business_objects.NewBooking;
+import com.epam.transavia.demo.business_objects.bo_factory.NewBookingFactory;
+import com.epam.transavia.demo.services.SearchFlightsService;
+import com.epam.transavia.demo.tests.steps.BaseTestBeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SearchFlightErrorsTests extends BaseTestBeforeClass {
+
+    @BeforeMethod()
+    public void beforeMethod() {
+        Assert.assertTrue(new SearchFlightsService().navigateToWhereToGoWindow(), "Where To Go window is not displayed, cannot proceed with further tests.");
+
+    }
+
+    @Test(description = "Verify that error message \"Unfortunately we do not fly from Dubai, United Arab Emirates to...\" is shown for the unsupported flight destination")
+    public void doNotFlyDestinationErrorShouldBeDisplayed() {
+        SearchFlightsService searchFlightsService = new SearchFlightsService();
+        NewBooking notFlyingBooking = NewBookingFactory.createNotFlyingBooking();
+
+        searchFlightsService.setFlyDestinations(notFlyingBooking);
+        searchFlightsService.searchDefaultOneWayOneUserFlight();
+
+        Assert.assertEquals(searchFlightsService.fetchBookingError(), notFlyingBooking.getBookingError(), "Error on Booking page does not meet to the expected: " + notFlyingBooking.getBookingError());
+    }
+}
